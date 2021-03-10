01bc30676ee5d4e9a9b28fc67247325409c202c5
irvine752 <irvine752@gmail.com>
2021-03-09 22:12:28 -0500

    initial submission

---
+++ Module_6_Solution/js/app.js
@@ -0,0 +1,49 @@
+(function () {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope) {
+    $scope.lunchMenuItems = "";
+    $scope.messageToDisplay = "";
+    $scope.processingComplete = false;
+
+    $scope.checkInput = function () {
+      if ($scope.lunchMenuItems.trim().length === 0) {
+         $scope.prefStyle = {
+          "color" : "red",
+          "border-color": "red"
+        }
+        $scope.emptyInput = true;
+      } else {
+
+        $scope.prefStyle = {
+          "color" : "green",
+          "border-color": "green"
+        }
+
+        $scope.processingComplete = true;
+        $scope.emptyInput = false;
+
+        var menuItems = $scope.lunchMenuItems.split(',');
+
+        for (var i = menuItems.length - 1; i >= 0; i--) {
+          if(menuItems[i].trim().length === 0){
+            menuItems.splice(i, 1)
+          }
+        }
+
+         console.log(menuItems);
+        
+        if (menuItems.length > 3) {
+          $scope.messageToDisplay = "Too much!";
+        } else {
+           $scope.messageToDisplay = "Enjoy!";
+        }
+      }
+    };
+  }
+})();
